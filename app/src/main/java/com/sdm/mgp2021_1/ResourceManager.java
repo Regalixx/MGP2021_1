d32ec14dd44e44b94b6ca0fa57ea30085fe0f058
Muhd Haidil <204122U@mymail.nyp.edu.sg>
2021-11-29 12:50:46 +0800

    player movement

--- app/src/main/java/com/sdm/mgp2021_1/ResourceManager.java
+++ app/src/main/java/com/sdm/mgp2021_1/ResourceManager.java
@@ -1,6 +1,7 @@
 package com.sdm.mgp2021_1;
 
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 
 import java.util.HashMap;
 
@@ -10,5 +11,11 @@ public class ResourceManager { //to load sprites
 
     private Resources res = null;
 
+    //  bmp = BitmapFactory.decodeResource(_view.getResources(),R.drawable.gamescene);
+    // Meant we need to retrieve the information from the _view, which is our surfaceview, and our image to be loaded on the view.
+
+    //We will use a Hashmap
+
+    private HashMap<Integer, Bitmap> resMap = new HashMap<Integer, Bitmap>();
 
 }
